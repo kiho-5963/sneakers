39fc3507fd55164e0c1a1abda73a874139381cd5
kiho-5963 <86970556+kiho-5963@users.noreply.github.com>
2023-05-25 09:20:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(function(){
+//  スマホメニュー
+
+  // ハンバーガーメニューのクリックイベント
+  // 解説は、「中級編：ストアサイト（インテリア）」参照
+  $('.hamburger').on('click', function() {
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
+    } else {
+      $('#header').addClass('open');
+    }
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $('#mask').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $('#nav a').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+// スクロール
+  // ページ内リンクのイベント
+  $('a[href^="#"]').click(function(){
+    // リンクを取得
+    let href= $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+
+// PICK UP スライダー
+  $('.slide').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '50px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+//フェードイン
+  $(window).scroll(function() {
+    // fadeinクラスに対して順に処理を行う
+    $('.fadein').each(function() {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
+});
